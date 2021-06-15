4f03b04c4df990764b1e5113ace9ef02c94a5aa5
unknown <lyudmila_ds@abv.bg>
2021-06-16 01:11:27 +0300

    Complete tic-tac-toe

--- WORKSHOP_Console_tic_tac_toe/tic_tac_toe/core/logic.py
+++ WORKSHOP_Console_tic_tac_toe/tic_tac_toe/core/logic.py
@@ -1,5 +1,10 @@
 from tic_tac_toe.core.validators import is_position_in_range, is_place_available
-from tic_tac_toe.core.helpers import get_row_col, print_current_board_state
+from tic_tac_toe.core.helpers import (
+    get_row_col,
+    print_current_board_state,
+    is_winner,
+    is_board_full
+)
 
 
 def play(players, board, turns):
@@ -8,13 +13,20 @@ def play(players, board, turns):
     while True:
         current_player_name = turns[current_turn_count % 2]
         position = int(input(f"{current_player_name} choose a free position: "))
-        if is_position_in_range(position):
-            if is_place_available(board, position):
-                row, col = get_row_col(position)
-                board[row][col] = players[current_player_name]
-                print_current_board_state(board)
-            # check if there is a winner
-        else:
-            pass
+        if current_turn_count >= 3:
+            if is_position_in_range(position):
+                if is_place_available(board, position):
+                    row, col = get_row_col(position)
+                    board[row][col] = players[current_player_name]
+                    print_current_board_state(board)
+                    if is_winner(board):
+                        print(f"{current_player_name} wins!")
+                        exit(0)
+                    if is_board_full(board):
+                        print("Game over! No winner!")
+                        exit(0)
+            else:
+                # read new position for the same player
+                pass
 
         current_turn_count += 1
