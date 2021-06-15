a4355c6a9dfb0c10d84e4a97e6249fdb7a4f437b
unknown <lyudmila_ds@abv.bg>
2021-06-15 11:15:54 +0300

    Added 'Mathematical operations' in Modules lab

---
+++ Modules/lab/mathematical_operations/constants.py
@@ -0,0 +1,9 @@
+from lab.mathematical_operations.operation_helper import *
+
+operation_mapper = {
+    "+": apply_sum,
+    "-": apply_subtract,
+    "*": apply_multiply,
+    "/": apply_divide,
+    "^": apply_power,
+}
---
+++ Modules/lab/mathematical_operations/operation_helper.py
@@ -0,0 +1,18 @@
+def apply_sum(x, y):
+    return f"{x + y:.2f}"
+
+
+def apply_subtract(x, y):
+    return f"{x - y:.2f}"
+
+
+def apply_divide(x, y):
+    return f"{x / y:.2f}"
+
+
+def apply_multiply(x, y):
+    return f"{x * y:.2f}"
+
+
+def apply_power(x, y):
+    return f"{x ** y:.2f}"
